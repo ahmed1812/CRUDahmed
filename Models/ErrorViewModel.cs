9a88733c2d5b9b594af2769e99f03b03efc47d07
ahmed1812 <algandel19@gmail.com>
2022-10-21 18:42:05 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUDahm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
