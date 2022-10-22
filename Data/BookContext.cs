6351f078dd44bf0ea07acf30e59be7bb3c290246
ahmed1812 <algandel19@gmail.com>
2022-10-21 19:05:53 -0500

    sql and server

---
+++ Data/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUDahm.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDahm.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+
+        }
+        public DbSet<BookList> BookList { get; set; }   
+    }
+}
---
+++ Models/BookList.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUDahm.Models
+{
+    public class BookList
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
