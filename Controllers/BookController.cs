90b147f0a62f7a601532fcf0cb177058c7799fd9
ahmed1812 <algandel19@gmail.com>
2022-10-21 21:23:46 -0500

    index

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUDahm.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CRUDahm.Controllers
 {
     public class BookController : Controller
     {
+        private readonly BookContext _context;
+
+        public BookController(BookContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var data = _context.BookList.ToList();
+            return View(data);
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace CRUDahm
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<BookContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BookContext")));
+            services.AddDbContext<BookContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
