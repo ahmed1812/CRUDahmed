89dcffb8aed5e0e0291e90105e030462f75582dd
ahmed1812 <algandel19@gmail.com>
2022-10-22 12:25:25 -0500

    create and edit

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,4 +1,5 @@
 ﻿using CRUDahm.Data;
+using CRUDahm.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
@@ -17,6 +18,42 @@ namespace CRUDahm.Controllers
         {
             var data = _context.BookList.ToList();
             return View(data);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(BookList item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(item);
+                _context.SaveChanges();
+            }
+            return View(item);
+        }
+        public IActionResult Edit(int id)
+        {
+            var book = _context.BookList.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(BookList item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(item);
+                _context.SaveChanges();
+            }
+            return View(item);
         }
     }
 }
